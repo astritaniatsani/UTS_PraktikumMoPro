4df4a3937fbd10c8bc93dba397ab0c8b85097e29
astritaniatsani <astritania05@gmail.com>
2019-11-04 04:03:33 +0700

    UTS

--- app/src/main/java/com/example/uts_11675037/MainActivity.java
+++ app/src/main/java/com/example/uts_11675037/MainActivity.java
@@ -13,11 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+   // RecyclerView list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        list = (RecyclerView) findViewById(R.id.recyclerview);
+//
+//        list.setLayoutManager(new LinearLayoutManager(this));
+//
+//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(MainActivity.this);
+//        list.setAdapter(adapter);
 
     }
 
